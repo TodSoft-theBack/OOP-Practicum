5e8d571b48bc84daddf30418d36101f6bf7a0a2a
Todor Todorov <63552782+TodSoft-theBack@users.noreply.github.com>
2023-05-11 16:20:45 +0000

    Added fuel tank class

--- Problems_04_05/CarClasses/FuelTank.cpp
+++ Problems_04_05/CarClasses/FuelTank.cpp
@@ -1,3 +1,17 @@
 #include "FuelTank.h"
+#include "insufficient_fuel_error.h"
 
 FuelTank::FuelTank(size_t capacity) : _capacity(capacity), _volume(capacity) { }
+
+void FuelTank::UseFuel(size_t amount)
+{
+    if (_volume - amount < 0)
+        throw insufficient_fuel_error();
+    _volume -= amount;
+    
+}
+
+void FuelTank::LoadFuel(size_t amount)
+{
+    _volume += amount;
+}
--- Problems_04_05/CarClasses/FuelTank.h
+++ Problems_04_05/CarClasses/FuelTank.h
@@ -5,7 +5,9 @@ class FuelTank
     private:
         size_t _capacity;
         size_t _volume;
-        
+
     public:
         FuelTank(size_t capacity);
+        void UseFuel(size_t amount);
+        void LoadFuel(size_t amount);
 };
